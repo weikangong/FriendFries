9f01ee2b19e61f0ddca4cfb98a1129f7e864b568
thegreatkang <ongwk95@hotmail.com>
2017-05-29 23:36:26 +0800

    Start Page!

---
+++ FriendFries/app/src/main/java/com/example/friendfries/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.friendfries;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startHost(View view) {
+        Intent intent = new Intent(this, HostActivity.class);
+        startActivity(intent);
+    }
+    public void startJoin(View view) {
+        Intent intent = new Intent(this, HostActivity.class);
+        startActivity(intent);
+    }
+    public void startSettings(View view) {
+        Intent intent = new Intent(this, HostActivity.class);
+        startActivity(intent);
+    }
+}
