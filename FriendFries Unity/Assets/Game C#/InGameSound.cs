fb2fb74f6574bf227ae839414103dfa69925dc92
thegreatkang <ongwk95@hotmail.com>
2017-08-09 14:59:07 +0800

    Added sound to the game!

---
+++ FriendFries Unity/Assets/Game C#/InGameSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameSound : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Play Global
+    private static InGameSound instance = null;
+    public static InGameSound Instance { get { return instance; } }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    //Play Gobal End
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ FriendFries Unity/Assets/Game C#/MenuSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSound : MonoBehaviour {
+    public AudioClip sound;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    // Use this for initialization
+    void Start () {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.Play();
+        InGameSound.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+}
